86d147c67a2685ef6ae537cde28be29fe559702a
Meir Elbaz <meire@flow-sec.com>
2025-04-14 17:01:42 +0300

    add test to arista eos

--- test_router_flowspec_parser.py
+++ test_router_flowspec_parser.py
@@ -6,6 +6,7 @@ from router_flowspec_parser import (
     Eq,
     FlowSpec,
     Fragment,
+    parse_flow_spec_arista_eos,
     parse_flow_spec_cisco_ios,
     parse_flow_spec_juniper_junos,
 )
@@ -43,6 +44,24 @@ AFI: IPv4
       Matched             :                  21/1968               
       Transmitted         :                  21/1968               
       Dropped             :                   0/0                  
+"""
+
+ARISTA_EOS_STDOUT = """
+Flow specification rules for VRF default
+Configured on: Ethernet1, Ethernet2
+Applied on: Ethernet1, Ethernet2
+  Flow-spec rule: 52.34.134.250/32;*;IP:=17;DP:=743;FRAG:2;
+    Rule identifier: 140278744221840
+    Matches:
+      Destination prefix: 52.34.134.250/32
+      Next protocol: 17
+      Destination port: 743
+      Fragment flags: is-fragment:1
+    Actions:
+      Drop
+    Status:
+      Installed: yes
+      Counter: 100 packets, 230 bytes
 """
 
 
@@ -135,4 +154,22 @@ def test_parse_flow_spec_cisco_ios():
             dropped_packets=0,
             action=Action.ACCEPT,
         ),
+    ]
+
+
+def test_parse_flow_spec_arista_eos():
+    entries = parse_flow_spec_arista_eos(ARISTA_EOS_STDOUT, "show flow-spec ipv4")
+
+    assert entries == [
+        FlowSpec(
+            dst_addr=IPv4Network("52.34.134.250/32"),
+            proto=[Eq(17)],
+            dst_port=[Eq(743)],
+            fragment=Fragment.IS_FRAGMENT,
+            matched_bytes=230,
+            matched_packets=100,
+            dropped_bytes=230,
+            dropped_packets=100,
+            action=Action.DISCARD,
+        )
     ]
